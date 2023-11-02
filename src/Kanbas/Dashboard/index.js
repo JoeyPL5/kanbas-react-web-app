dde46694cbe7e21ccfa5fcb34bc961dcec143c5b
Joseph Pierre-Louis <pierre-louis.jo@northeastern.edu>
2023-11-02 00:26:21 -0400

    safety

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,93 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
-import { React, useState } from "react";
+import CourseCard from "./CourseCard";
+import data from "../Database"; 
 
 function Dashboard() {
-  const [courses, setCourses] = useState(db.courses);
-  const [course, setCourse] = useState({
-    name: "New Course",
-    number: "New Number",
-    startDate: "2023-09-10",
-    endDate: "2023-12-15",
-  });
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
-  };
-
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime() }]);
-  };
-  const updateCourse = () => {
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
-  };
+  const { courses } = data;
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <button onClick={updateCourse}>Update</button>
-
-      <button onClick={addNewCourse}>Add</button>
-      <h5>Course</h5>
-      <input
-        value={course.name}
-        className="form-control"
-        onChange={(e) => setCourse({ ...course, name: e.target.value })}
-      />
-      <input
-        value={course.number}
-        className="form-control"
-        onChange={(e) => setCourse({ ...course, number: e.target.value })}
-      />
-      <input
-        value={course.startDate}
-        className="form-control"
-        type="date"
-        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
-      />
-      <input
-        value={course.endDate}
-        className="form-control"
-        type="date"
-        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
-      />
-
-      <div className="list-group">
-        {courses.map((course) => (
-          <Link
-            key={course._id}
-            to={`/Kanbas/Courses/${course._id}`}
-            className="list-group-item"
-          >
-            <button
-              onClick={(event) => {
-                event.preventDefault();
-                deleteCourse(course._id);
-              }}
-            >
-              Delete
-            </button>
-
-            <button
-              onClick={(event) => {
-                event.preventDefault();
-                setCourse(course);
-              }}
-            >
-              Edit
-            </button>
-
-            {course.name}
-          </Link>
-        ))}
+    <div className="container">
+      <div className="row justify-content-start">
+        <div className="col-md-9">
+          <h1 className="black-title">
+            <span style={{ fontWeight: "normal" }}>Dashboard</span>
+          </h1>
+          <hr />
+          <h2 className="black-title">
+            <span style={{ fontWeight: "normal", marginLeft: "25px" }}>
+              Published Courses ({courses.length})
+            </span>
+          </h2>
+          <hr style={{ marginLeft: "25px" }} />
+          <div className="d-flex flex-row flex-wrap" style={{ marginLeft: "25px" }}>
+            {courses.map((course) => (
+              <Link to={`/Kanbas/Courses/${course._id}`} key={course._id}>
+                <CourseCard course={course} />
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Labs/store/index.js
+++ src/Labs/store/index.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../a4/ReduxExamples/HelloRedux/helloReducer";
 import counter1Reducer from "../a4/ReduxExamples/Counter1/counter1Reducer";
 import counter2Reducer from "../a4/ReduxExamples/Counter2/counter2Reducer";
+import counterReducer from "../a4/ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "../a4/ReduxExamples/AddRedux/addReducer";
 import todosReducer from "../a4/ReduxExamples/todos/todosReducer";
 
@@ -9,6 +10,7 @@ const store = configureStore({
   reducer: {
     todosReducer,
     helloReducer,
+    counterReducer,
     counter1Reducer,
     counter2Reducer,
     addReducer,
